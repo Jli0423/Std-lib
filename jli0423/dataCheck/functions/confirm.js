465bc99c75eb99f47aef1ccf8cf2aa7423d701a4
Justin Li <justin.li0423@gmail.com>
2018-02-03 15:55:42 -0500

    DB API Prototype

--- jli0423/dataCheck/functions/confirm.js
+++ jli0423/dataCheck/functions/confirm.js
@@ -3,20 +3,46 @@ const mongoose = require('mongoose');
 
 /**
 * A basic Hello World function
-* @param {string} name Who you're saying hello to
-* @returns {string}
+  @param {object} data
+* @returns {any}
 */
-module.exports = async (name = 'world', context, callback) => {
+module.exports = async (data = '', context) => {
 
-  var testSchema = mongoose.Schema({
-    name: String
+  // if(!data) {
+  //   return {
+  //     "err_code": 404
+  //   }
+  // };
+
+  var receiptSchema = mongoose.Schema({
+    type: String,
+    amount: Number,
+    items:[]
   });
+
+  var test = mongoose.model('test', receiptSchema);
+  var receipt = new test({
+    type: "Superstore",
+    amount: 49.42,
+    items: [{
+      "Item 1": 38.34,
+      "Item 2": 32.12,
+      "Item 3": 2.43,
+      "NANI": 0.00
+    }]
+  });
+
+
 
   // DB Connection
   var mongoURI = 'mongodb://qhacks2018:qhacks2018@ds046357.mlab.com:46357/receipt-data';
   await mongoose.connect(mongoURI);
-  console.log(mongoose)
-
-  callback(null, `hello ${name}`);
+  console.log(receipt);
+  receipt.save((err, res) => {
+    if(err) {
+      return console.log(err);
+    }
+  });
+  return "done!";
 
 };
