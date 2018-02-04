1a411310ccbfb3978aa1debadf20730e6a4a8655
Justin Li <justin.li0423@gmail.com>
2018-02-04 02:01:03 -0500

    Added bandaid api

--- jli0423/image-api/functions/image.js
+++ jli0423/image-api/functions/image.js
@@ -1,5 +1,6 @@
 const lib = require('lib');
 const imgur = require('imgur');
+const request = require('request');
 /**
  * @param {string} imgurID Who you're saying hello to
  * @returns {any}
@@ -41,13 +42,23 @@ module.exports = (imgurID = '', context, callback) => {
   imgur.loadClientId().then(imgur.setClientId);
   imgur.getInfo(imgurID).then((json) => {
     if (json.status == 200) {
-      return callback(null, {
-        "pic_url": json.data.link,
-        "items": testObject,
-        "total": total,
-        "type": "Temporary",
-        "err_code": json.status
+      request({
+        url: "https://bandaidapi.herokuapp.com/getData",
+        method: "POST",
+        json: true,
+        body: {
+          "id": imgurID
+        }
+      }, (err, res, body) => {
+        return callback(null, res);
       });
+      // return callback(null, {
+      //   "pic_url": json.data.link,
+      //   "items": testObject,
+      //   "total": total,
+      //   "type": "Temporary",
+      //   "err_code": json.status
+      // });
     }
   }).catch((err) => {
     return callback(null, err.status);
